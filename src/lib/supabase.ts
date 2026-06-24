00f7e48d7e7d2fab67b153fce8b15ca9f9f28f2b
jlladro <lladrovcijon@gmail.com>
2026-06-24 02:00:33 +0200

    Vercel Sperre umgehen

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -1,7 +1,7 @@
 import { createClient } from '@supabase/supabase-js'
 
-// DER RICHTIGE SCHLÜSSEL - Endlich geschafft!
-const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL || 'https://dqgaejjdiggdwmcsmyju.supabase.co'
-const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY || 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImRxZ2FlampkaWdnZHdtY3NteWp1Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODIyMTkxMDAsImV4cCI6MjA5Nzc5NTEwMH0.K0oI7oh6gmUQGwk6WzsuMQKxcAXqDsZd4GeRchbbuW0'
+// Wir nutzen neue Namen, damit Vercel unsere Schlüssel nicht mehr mit alten Werten überschreiben kann.
+const supabaseUrl = 'https://dqgaejjdiggdwmcsmyju.supabase.co'
+const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImRxZ2FlampkaWdnZHdtY3NteWp1Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODIyMTkxMDAsImV4cCI6MjA5Nzc5NTEwMH0.K0oI7oh6gmUQGwk6WzsuMQKxcAXqDsZd4GeRchbbuW0'
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey)
